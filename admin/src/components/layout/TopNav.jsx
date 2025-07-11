e3e071046766fd2356911238f9ce743e3cff277f
manish madhu <manishmadhu53@gmail.com>
2025-07-11 12:34:39 +0530

    nav and home page changes

--- admin/src/components/layout/TopNav.jsx
+++ admin/src/components/layout/TopNav.jsx
@@ -22,7 +22,7 @@ const TopNav = ({ onMenuClick }) => {
             <FiMenu className="w-6 h-6" />
           </button> */}
           
-          <div className="relative mx-4">
+          {/* <div className="relative mx-4">
             <span className="absolute inset-y-0 left-0 flex items-center pl-3">
               <FiSearch className="text-gray-400" />
             </span>
@@ -31,7 +31,7 @@ const TopNav = ({ onMenuClick }) => {
               placeholder="Search..."
               className="w-64 pl-10 pr-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
             />
-          </div>
+          </div> */}
         </div>
 
         <div className="flex items-center space-x-4">
